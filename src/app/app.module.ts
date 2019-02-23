c56b23c2820df0acca00f5462f1ac61ec5e09ced
satishGangineni <satish.neni@gmail.com>
2019-02-23 09:31:05 +0530

    header footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    ShoppingListComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
